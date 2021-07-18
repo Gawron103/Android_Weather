b067700c134a3ba082b75c82acfca00c00b5d85d
Piotrek <gawron103@gmail.com>
2021-07-18 13:41:33 +0200

    Added navigation drawer

--- Weather/app/src/main/java/com/example/weather/views/MainActivity.kt
+++ Weather/app/src/main/java/com/example/weather/views/MainActivity.kt
@@ -2,19 +2,58 @@ package com.example.weather.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.weather.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toolbar: Toolbar? = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawLayout, toolbar, R.string.open, R.string.close)
+        drawLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navView = findViewById<NavigationView>(R.id.navView)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.menuCurrentWeather -> Toast.makeText(applicationContext,
+                    "Clicked current weather menu item", Toast.LENGTH_SHORT).show()
+
+                R.id.menuForecastForCurrent -> Toast.makeText(applicationContext,
+                    "Clicked forecast menu item", Toast.LENGTH_SHORT).show()
+
+                R.id.menuChangeLocation -> Toast.makeText(applicationContext,
+                    "Clicked change location menu item", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
